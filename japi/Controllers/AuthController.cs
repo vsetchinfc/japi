6bad089c211b810cce8e2c2b70b3aad20c5ec10d
vsetchinfc <vlad@setchinfc.com.au>
2018-09-20 15:19:45 +1000

    Replacing simple Auth with JWT

---
+++ japi/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JApi.Controllers
+{
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        // GET api/values
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody]User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            if (user.UserName == "testuser1" && user.Password == "testuser1")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: "http://localhost:5000",
+                    audience: "http://localhost:5000",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signinCredentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                return Ok(new { Token = tokenString });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+
+}
--- japi/Startup.cs
+++ japi/Startup.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
 
 using JApi.DAL;
 using JApi.Models;
-using JApi.Middleware;
 
 namespace JApi
 {
@@ -28,8 +31,32 @@ namespace JApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = "http://localhost:5000",
+                    ValidAudience = "http://localhost:5000",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
+                };
+            });
+
             services.AddMvc();
             services.AddTransient(typeof(IRepository<Product>), typeof(ProductRepository));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("EnableCORS", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials().Build();
+                });
+            });
 
             /*Adding swagger generation with default settings*/
             services.AddSwaggerGen(c =>
@@ -53,9 +80,10 @@ namespace JApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMiddleware<AuthenticationMiddleware>();
-
             app.UseMvc();
+
+            app.UseCors("EnableCORS");
+            app.UseAuthentication();            
 
             /*Enabling swagger file*/
             app.UseSwagger();
